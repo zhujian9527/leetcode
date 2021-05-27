61a704a3b5acd3786b87b494e88a6c9e74a8ab99
ekko <zjekko@163.com>
2021-05-27 09:55:13 +0800

    test

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -1,3 +1,5 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -7,7 +9,7 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+            System.out.println("hello");
     }
 }
 // @lc code=end
